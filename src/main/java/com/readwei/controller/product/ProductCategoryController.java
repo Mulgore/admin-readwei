233b8ae91b3df4635c3ed3bb325366a1debfb8ee
xingwu <wangyu19930107@qq.com>
2017-03-21 17:44:33 +0800

    完善商品分类，修改，删除

--- src/main/java/com/readwei/controller/product/ProductCategoryController.java
+++ src/main/java/com/readwei/controller/product/ProductCategoryController.java
@@ -4,8 +4,10 @@ import com.baomidou.kisso.annotation.Permission;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.readwei.controller.sys.BaseController;
+import com.readwei.entity.Product;
 import com.readwei.entity.ProductCategory;
 import com.readwei.service.IProductCategoryService;
+import com.readwei.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,6 +31,8 @@ public class ProductCategoryController extends BaseController {
 
     @Autowired
     private IProductCategoryService productCategoryService;
+    @Autowired
+    private IProductService productService;
 
     /**
      * 分类列表页面
@@ -84,10 +88,63 @@ public class ProductCategoryController extends BaseController {
         category.setModifyTime(new Date());
         rlt = productCategoryService.insert(category);
         if (!rlt) {// 添加失败的时候，返回值为fasle，给前端传递一个message
-            return callbackFail("商品分类添加失败!!!");
+            return callbackFail("商品分类添加失败！！！");
         }
         return callbackSuccess(rlt);
     }
 
+    /**
+     * 删除商品分类实现方法
+     *
+     * @param id 商品分类Id
+     * @return
+     */
+    @Permission("5002")
+    @RequestMapping(value = "/del", method = RequestMethod.POST)
+    @ResponseBody
+    public String categoryDel(Integer id) {
+        boolean rlt = false;
+        ProductCategory category = productCategoryService.selectById(id); // 判断分类石否存在
+        if (category == null) {
+            return callbackFail("商品分类不存在！！！");
+        }
+        EntityWrapper<Product> wrapper = new EntityWrapper<Product>();
+        Product product = new Product();
+        product.setCategoryId(id);
+        wrapper.setEntity(product);
+        int count = productService.selectCount(wrapper); // 判断商品分类是否在使用
+        if (count > 0) {
+            return callbackFail("商品分类正在使用！！！");
+        }
+        rlt = productCategoryService.deleteById(id); // 删除分类实现
+        if (!rlt) {
+            return callbackFail("商品分类删除失败！！！");
+        }
+        return callbackSuccess(rlt);
+    }
+
+    /**
+     * 修改商品分类页面
+     *
+     * @return
+     */
+    @Permission("5002")
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String categoryEditView() {
+        return "product/category/edit";
+    }
+
+    /**
+     * 查询商品分类详情
+     *
+     * @param id
+     * @return
+     */
+    @Permission("5002")
+    @RequestMapping(value = "/info", method = RequestMethod.POST)
+    @ResponseBody
+    public String categoryInfo(Integer id) {
+        return toJson(productCategoryService.selectById(id));
+    }
 
 }
