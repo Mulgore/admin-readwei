bab739fc73af90a521a7a3d538ed9a8632f5f17d
xingwu <wangyu19930107@qq.com>
2017-04-29 00:35:48 +0800

    updaye

--- src/main/java/com/reawei/controller/address/AddressController.java
+++ src/main/java/com/reawei/controller/address/AddressController.java
@@ -1,11 +1,17 @@
 package com.reawei.controller.address;
 
 import com.baomidou.kisso.annotation.Permission;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.reawei.controller.sys.BaseController;
+import com.reawei.entity.RwAddress;
+import com.reawei.service.IRwAddressService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -19,16 +25,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/address")
 public class AddressController extends BaseController {
 
+    @Resource
+    private IRwAddressService addressService;
+
+    /**
+     * 用户地址列表页面
+     * @return
+     */
     @Permission("9001")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String listView() {
         return "address/list";
     }
 
+    /**
+     * 查询用户地址列表
+     * @return
+     */
     @Permission("9001")
     @RequestMapping(value = "/getList", method = RequestMethod.GET)
     @ResponseBody
     public String getList() {
-        return jsonPage(null);
+        Page<RwAddress> page = getPage();
+        EntityWrapper<RwAddress> wrapper = new EntityWrapper<>();
+        RwAddress address = new RwAddress();
+        wrapper.setEntity(address);
+        page = addressService.selectPage(page,wrapper);
+        return jsonPage(page);
     }
 }
--- src/main/java/com/reawei/controller/express/ExpressController.java
+++ src/main/java/com/reawei/controller/express/ExpressController.java
@@ -1,11 +1,17 @@
 package com.reawei.controller.express;
 
 import com.baomidou.kisso.annotation.Permission;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.reawei.controller.sys.BaseController;
+import com.reawei.entity.RwExpress;
+import com.reawei.service.IRwExpressService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -19,17 +25,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/express")
 public class ExpressController extends BaseController {
 
+    @Resource
+    private IRwExpressService expressService;
+
+
+    /**
+     * 物流列表页面
+     * @return
+     */
     @Permission("8001")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String listView() {
         return "/express/list";
     }
 
+    /**
+     * 查询物流列表
+     * @return
+     */
     @Permission("8001")
     @RequestMapping(value = "/getList", method = RequestMethod.GET)
     @ResponseBody
     public String getList() {
-        return jsonPage(null);
+        Page<RwExpress> page = getPage();
+        EntityWrapper<RwExpress> wrapper = new EntityWrapper<>();
+        RwExpress express = new RwExpress();
+        wrapper.setEntity(express);
+        page = expressService.selectPage(page,wrapper);
+        return jsonPage(page);
     }
 
 }
