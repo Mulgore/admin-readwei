d696f7bc6ca6771bf6e221b8ae93badfe51aaddb
xingwu <wangyu19930107@qq.com>
2017-05-11 22:14:41 +0800

    修改天气数据

--- src/main/java/com/reawei/controller/sys/EchartsController.java
+++ src/main/java/com/reawei/controller/sys/EchartsController.java
@@ -37,34 +37,37 @@ public class EchartsController extends BaseController {
     public String map(Model model) {
         String ipAddress = IpHelper.getIpAddr(this.request);
 //        String ipAddress = "123.206.181.24";
-        Map<String, String> lonLatData = GetPlaceByIp.getlonLatByIP(ipAddress);
         String city = "";
         List<Integer> lowestTemperature = new ArrayList<>();
         List<Integer> maximumTemperature = new ArrayList<>();
         List<String> weeks = new ArrayList<>();
-        if (lonLatData != null) {
-            Map<String, Object> weatherData = WeatherUtil.weekWeadTher(lonLatData.get("lon"), lonLatData.get("lat"));
-            if (weatherData != null) {
-                logger.info("===================== { today weather= " + weatherData.get("today") + " } ==============");
-                logger.info("===================== { future weather= " + weatherData.get("future") + " } ==============");
-                JSONObject today = (JSONObject) weatherData.get("today");
-                JSONArray future = (JSONArray) weatherData.get("future");
-                city = today.get("city").toString();
-                weeks.add('\'' + today.get("week").toString() + '\'');
-                String[] todayTemperature = today.get("temperature").toString().split("~");
-                lowestTemperature.add(Integer.parseInt(todayTemperature[0]));
-                maximumTemperature.add(Integer.parseInt(todayTemperature[1]));
-                int i = 0;
-                for (Object arr : future) {
-                    if (i < 6) {
-                        JSONObject val = (JSONObject) arr;
-                        String[] temperatures = val.get("temperature").toString().split("~");
-                        lowestTemperature.add(Integer.parseInt(temperatures[0]));
-                        maximumTemperature.add(Integer.parseInt(temperatures[1]));
-                        weeks.add('\'' + val.get("week").toString() + '\'');
-                        i++;
-                    }
+        if (!"127.0.0.1".equals(ipAddress)) {
+            Map<String, String> lonLatData = GetPlaceByIp.getlonLatByIP(ipAddress);
 
+            if (lonLatData != null) {
+                Map<String, Object> weatherData = WeatherUtil.weekWeadTher(lonLatData.get("lon"), lonLatData.get("lat"));
+                if (weatherData != null) {
+                    logger.info("===================== { today weather= " + weatherData.get("today") + " } ==============");
+                    logger.info("===================== { future weather= " + weatherData.get("future") + " } ==============");
+                    JSONObject today = (JSONObject) weatherData.get("today");
+                    JSONArray future = (JSONArray) weatherData.get("future");
+                    city = today.get("city").toString();
+                    weeks.add('\'' + today.get("week").toString() + '\'');
+                    String[] todayTemperature = today.get("temperature").toString().split("~");
+                    lowestTemperature.add(Integer.parseInt(todayTemperature[0]));
+                    maximumTemperature.add(Integer.parseInt(todayTemperature[1]));
+                    int i = 0;
+                    for (Object arr : future) {
+                        if (i < 6) {
+                            JSONObject val = (JSONObject) arr;
+                            String[] temperatures = val.get("temperature").toString().split("~");
+                            lowestTemperature.add(Integer.parseInt(temperatures[0]));
+                            maximumTemperature.add(Integer.parseInt(temperatures[1]));
+                            weeks.add('\'' + val.get("week").toString() + '\'');
+                            i++;
+                        }
+
+                    }
                 }
             }
         }
