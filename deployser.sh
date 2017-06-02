3a0353db51e32c97abb71c361d5912b9b66c84f7
xingwus <wangyu19930107@qq.com>
2017-06-02 17:53:07 +0800

    修改服务器用户

--- deployser.sh
+++ deployser.sh
@@ -1,2 +1,2 @@
 mvn clean package -Dmaven.test.skip -Prelease
-scp -r target/root.war root@123.206.181.24:/home/admin/jetty/webapps/
+scp -r target/root.war admin@123.206.181.24:/home/admin/jetty/webapps/
